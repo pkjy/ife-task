5878e90e178ff0616408129ff303b4378061898b
pkjy <a214161398@hotmail.com>
2018-03-24 15:36:15 +0800

    更新dist路径

---
+++ 2016_spring/part4/task50/task50_vue/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  baseUrl: "./",
+  devServer: {
+    open: process.platform === "darwin",
+    host: "0.0.0.0",
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+    proxy: null, // string | Object
+    before: app => {}
+  }
+}
